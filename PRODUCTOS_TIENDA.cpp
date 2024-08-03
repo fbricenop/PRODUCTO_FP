4f422559408d92778d81d093c3e5ca249b54d941
arramosm13 <arramosm@unjbg.edu.pe>
2024-08-03 17:07:39 -0500

    Comenze el programa con una funcion para que el titulo de la tienda se vea llamativo.

---
+++ PRODUCTOS_TIENDA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void mostrarTitulo(const string& titulo) {
+    int longitud = titulo.length();
+
+    for (int i = 0; i < longitud + 4; ++i) {
+        cout << "*";
+    }
+    cout << endl;
+    cout << "* " << titulo << " *" << endl;
+    for (int i = 0; i < longitud + 4; ++i) {
+        cout << "*";
+    }
+    cout << endl;
+}
