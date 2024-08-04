d1220fe009ffd47f442a0f97f40d67c738bab1de
fernando <fbricenop@unjbg.edu.pe>
2024-08-04 17:07:45 -0500

    generacion de un numero aletorio a modo de medida de seguridad para el usuario que quiera acceder como administrador de la tienda

---
+++ comprobacion_xnumero.h
@@ -0,0 +1,20 @@
+#include "libreriascomunes.h"
+using namespace std;
+
+void numaleatorio(int a){
+    int s,intento;
+    for(int i=0; i<3;i++){
+        s= rand()%(200 +1 -0);
+        cout<<"DIGITE EL SIGUIENTE NUMERO ALEATORIO : ";
+        cout<<s<<endl;
+        cout<<"ACÁ EL NUMERO: ";
+        cin>>intento;
+        if(intento==s){
+            cout<<"ULTIMO CHEKEO COMPLETADO"<<endl;
+            i=3;
+            }else if(i==2){
+                cout<<"INTENTOS ACABADOS"<<endl;
+                //regresar a la interfaz de usario
+            }
+        }
+}
