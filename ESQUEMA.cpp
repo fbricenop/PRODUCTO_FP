8702ca9601f0a6aa35c2cf566f39aa48aa9c94ac
fernando <fbricenop@unjbg.edu.pe>
2024-08-04 20:32:44 -0500

    avance de la interfaz del usuario

--- ESQUEMA.cpp
+++ ESQUEMA.cpp
@@ -1,17 +1,19 @@
 #include "libreriascomunes.h"
 #include "comprobacion.h"
+#include "usuario.h"
 //lamada a archivos .h
 //#include "comprobacion.h";
 using namespace std;
 int main(){
     srand(time(0));
     int categoria,opcion_administrador;
-    cout<<"HOLA USUARIO ";  
+    g();
+    
     /*
     interfaz de usuario
     */
     //UNA VEZ TERMINE EL BUCLE DEL USUARIO AL PRESIONAR "CUENTA DIFERNTE".
-    cout<<"__ESCRIBA {1} SI ES ADMINISTRADOR O {0} PARA SALIR__";//tambien puede ser 0 para volver a interfz usuario;
+    cout<<endl<<"__ESCRIBA {1} SI ES ADMINISTRADOR O {0} PARA SALIR__";//tambien puede ser 0 para volver a interfz usuario;
     cin>>categoria;
     comprobacion_administrador(categoria);
     //int comprobacion_administrador(int categoria);
