d593fa0243d01981c80562de65e34b6aab8320e6
abfloresc <abfloresc@unjbg.edu.pe>
2024-08-09 10:14:29 -0500

    Create realizarpago.h

---
+++ realizarpago.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "funtarjetaoefec.h"
+
+using namespace std;
+
+void realizarpago(double totalcompra) {
+    cout << "Total a pagar: $" << totalcompra << endl;
+    cout << "Gracias por su compra." << endl;
+}
