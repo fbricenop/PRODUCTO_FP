634aa71d642636b6c36f6ddc348e122079bbc2ba
abfloresc <abfloresc@unjbg.edu.pe>
2024-08-09 10:15:47 -0500

    Create mostrarbeneficios.h

---
+++ mostrarbeneficios.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "funtarjetaoefec.h"
+
+using namespace std;
+
+void mostrarbeneficios() {
+    cout << "Ahora que eres miembro, disfrutas de un 10% en todas tus compras usando la tarjeta de la tienda." << endl;
+}
